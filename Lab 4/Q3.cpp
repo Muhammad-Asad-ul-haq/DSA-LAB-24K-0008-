7dc58655164a801441d74b9782fa2f53e148f2aa
Muhammad-Asad-ul-haq <asadhaq01@gmail.com>
2025-09-30 14:47:57 +0500

    Q3.cpp

---
+++ Lab 4/Q3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string arr[] = {"banana", "apple", "cherry", "date", "grape"};
+    int n = 5, i, j;
+    string temp;
+
+    for(i = 0; i < n - 1; i++) {
+        for(j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    for(i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
